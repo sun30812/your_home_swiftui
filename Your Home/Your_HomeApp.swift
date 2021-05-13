6faa431640cbfbfe3b414c3d9ffa88e68da6e2a1
sun30812 <sun30812@naver.com>
2021-05-13 17:25:21 +0900

    Initial Commit

---
+++ Your Home/Your_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Your_HomeApp.swift
+//  Your Home
+//
+//  Created by 이정민 on 2021/05/13.
+//
+
+import SwiftUI
+
+@main
+struct Your_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
